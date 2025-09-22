65722be5821226cf1d8ccc53c84ccdc9624d3ec4
nushams <shamshidenn@gmail.com>
2025-09-22 07:23:53 +0200

    fix policy module

--- terraform-modules/policy/main.tf
+++ terraform-modules/policy/main.tf
@@ -1,15 +1,15 @@
-resource "azurerm_policy_definition" "tagpolicy" {
-  for_each = { for k, v in var.policy_definition : k => v }
+# resource "azurerm_policy_definition" "tagpolicy" {
+#   for_each = { for k, v in var.policy_definition : k => v }
 
-  name         = each.value.metadata.slug
-  policy_type  = "Custom"
-  mode         = each.value.mode
-  display_name = each.value.displayName
-  description  = each.value.description
-  metadata     = jsonencode(each.value.metadata)
-  policy_rule  = jsonencode(each.value.policyRule)
-  parameters   = contains(keys(each.value), "parameters") ? jsonencode(each.value.parameters) : null
-}
+#   name         = each.value.metadata.slug
+#   policy_type  = "Custom"
+#   mode         = each.value.mode
+#   display_name = each.value.displayName
+#   description  = each.value.description
+#   metadata     = jsonencode(each.value.metadata)
+#   policy_rule  = jsonencode(each.value.policyRule)
+#   parameters   = contains(keys(each.value), "parameters") ? jsonencode(each.value.parameters) : null
+# }
 
 resource "azurerm_policy_definition" "locationpolicy" {
   name         = "allowedlocations"
@@ -65,6 +65,64 @@ resource "azurerm_policy_definition" "locationpolicy" {
   PARAMETERS
 }
 
+resource "azurerm_policy_definition" "tagpolicy" {
+  name         = "mandatory-tags"
+  policy_type  = "Custom"
+  mode         = "Indexed"
+  display_name = "Enforce Mandatory Tags for Resource Groups"
+  description  = "This policy ensures that all Resource Groups have an 'env' tag with allowed values."
+
+  metadata = <<METADATA
+{
+  "category": "General",
+  "slug": "mandatory-tags"
+}
+METADATA
+
+  policy_rule = <<POLICY_RULE
+{
+  "if": {
+    "allOf": [
+      {
+        "field": "type",
+        "equals": "Microsoft.Resources/subscriptions/resourceGroups"
+      },
+      {
+        "not": {
+          "field": "tags['env']",
+          "in": "[parameters('env')]"
+        }
+      }
+    ]
+  },
+  "then": {
+    "effect": "deny"
+  }
+}
+POLICY_RULE
+
+  parameters = <<PARAMETERS
+{
+  "env": {
+    "type": "Array",
+    "metadata": {
+      "displayName": "Environment",
+      "description": "List of allowed environment tags"
+    },
+    "defaultValue": [
+      "dev"
+    ],
+    "allowedValues": [
+      "dev",
+      "prod",
+      "test",
+      "onprem"
+    ]
+  }
+}
+PARAMETERS
+}
+
 resource "azurerm_subscription_policy_assignment" "locationpolicy" {
   name                 = "allowed-locations-policy-assignment"
   description          = "Policy Assignment created for Allowed Locations"
@@ -72,10 +130,17 @@ resource "azurerm_subscription_policy_assignment" "locationpolicy" {
   subscription_id      = "/subscriptions/${var.subscription_id}"
   parameters = jsonencode({
     "listOfAllowedLocations" : {
-      "value" : var.location,
+      "value" : var.allowed_locations,
     }
     }
   )
+}
+
+resource "azurerm_subscription_policy_assignment" "tagpolicy" {
+  name                 = "tag-policy-assignment"
+  description          = "Policy Assignment for Tags"
+  policy_definition_id = azurerm_policy_definition.tagpolicy.id
+  subscription_id      = "/subscriptions/${var.subscription_id}"
 }
 
 resource "azurerm_subscription_policy_assignment" "allowedvmsku" {
@@ -95,11 +160,4 @@ resource "azurerm_subscription_policy_assignment" "allowedvmsku" {
       ]
     }
   })
-}
-
-resource "azurerm_key_vault_access_policy" "kvpolicy" {
-  key_vault_id    = var.key_vault_id
-  tenant_id       = var.tenant_id
-  object_id       = var.managed_identity
-  key_permissions = var.keypermissionspolicy
 }
--- terraform-modules/policy/variables.tf
+++ terraform-modules/policy/variables.tf
@@ -1,13 +1,32 @@
-variable "policy_definition" {}
+# variable "policy_definition" {}
 
-variable "subscription_id" {}
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
 
-variable "location" {}
+variable "allowed_locations" {
+  type        = list(any)
+  description = "List of allowed Locations"
+  default     = ["eastus"]
+}
 
-variable "tenant_id" {}
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
 
-variable "key_vault_id" {}
+variable "key_vault_id" {
+  type        = string
+  description = "Key Vault ID"
+}
 
-variable "managed_identity" {}
+variable "workload_identity" {
+  type        = string
+  description = "AKS Workload Identity"
+}
 
-variable "keypermissionspolicy" {}
+variable "keypermissionspolicy" {
+  type    = list(string)
+  default = ["Create", "Get", "Delete", "Purge", "Recover", "Update", "GetRotationPolicy", "SetRotationPolicy"]
+}
